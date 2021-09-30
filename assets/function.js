bed3bc7b3e8b1c3269c11373b279fdaa7c6c93b2
Juan Carlos Cárdenas <jc.cardenas.perez@gmail.com>
2021-09-30 13:15:40 -0500

    a
    a

--- assets/function.js
+++ assets/function.js
@@ -5,20 +5,41 @@ fetch("../assets/response_marvel.json")
             const element = datos.data.results[index];
             for (let j = 0; j < element.comics.items.length; j++) {
                 const comic = element.comics.items[j];
-                let container = `<div class="container-comic grid mt-20">
+                let container = `<div class="container-comic grid mt-20 trans-opacity">
                                     <div class="spacing d-flex-aic">
                                         <img src="https://via.placeholder.com/300/09f/${element.thumbnail.path+'.'+element.thumbnail.extension}" alt="heroe-cover" class="box-img">
                                         <div class="pl-space">
                                             <h4 class="fs-20">${element.name}</h4>
-                                            <span class="fs-15 name">${comic.name}</span>
+                                            ${ comic.name.includes('Captain America') ? `<span class="fs-15 name font-muli-bold">${comic.name}</span>` : `<span class="fs-15">${comic.name}</span>`}
                                         </div>
                                     </div>
                                     <div class="box-delete text-center pointer">
                                         <span class="style-cross">X</span>
                                     </div>
-                                    <span class="style-cross pos-delete delete${j} pointer">Eliminar</span>
+                                    <span class="style-cross pos-delete delete">Eliminar</span>
                                 </div>`
                 document.querySelector('.padding-main-section').innerHTML += container;
+                let boxDelete = document.querySelectorAll('.box-delete');
+                let btnDelete = document.querySelectorAll('.delete');
+                boxDelete.forEach(element => {
+                    element.addEventListener('click', function(e) {
+                        if(element.parentElement.offsetWidth === 1090) {
+                             element.parentElement.style.width='1000px';
+                             this.firstElementChild.textContent='>';
+                             element.parentElement.children[2].style.zIndex='1';
+                        } else {
+                             element.parentElement.style.width='1090px';
+                             this.firstElementChild.textContent='X';
+                             element.parentElement.children[2].style.zIndex='-1';
+                        }
+                     })
+                });
+                btnDelete.forEach(element => {
+                    element.addEventListener('click', function(e) {
+                        element.style.opacity = '0';
+                        e.target.parentElement.remove();
+                    })
+                })
             }
         }
     })
